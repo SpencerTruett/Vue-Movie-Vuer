f45370b4f3216102d8789cceac3e828f6f0feeef
Spencer Truett <srtruett1993@gmail.com>
2021-03-15 10:26:01 -0500

    styled the header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,26 @@
 <template>
   <v-app-bar app color="red darken-1" dark>
-    <h1>Movie Vuer</h1>
-    <form @submit.prevent="handleSubmit" style="background: white">
-      <input type="text" v-model="query" />
-    </form>
+    <v-row justify="space-between" align="center">
+      <v-col :sm="6">
+        <div class="d-flex align-center">
+          <h1>Movie Vuer</h1>
+        </div>
+      </v-col>
+
+      <v-col :sm="2" class="mt-5">
+        <form @submit.prevent="handleSubmit">
+          <v-text-field
+            v-model="query"
+            dense
+            rounded
+            filled
+            placeholder="Search movie titles"
+            background-color="red lighten-2"
+            prepend-inner-icon="mdi-magnify"
+          ></v-text-field>
+        </form>
+      </v-col>
+    </v-row>
   </v-app-bar>
 </template>
 
